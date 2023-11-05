b5042b6fe0155a1c6420cd663c584f8077811a6c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-05 16:13:10 +0600

    Explored motion basics

--- src/components/BasicsOfFramerMotion.jsx
+++ src/components/BasicsOfFramerMotion.jsx
@@ -1,6 +1,44 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+
 export default function BasicsOfFramerMotion() {
+	const [isVisible, setIsVisible] = useState(true);
 	return (
-		<div
-			style={{ height: 150, width: 150, backgroundColor: 'black' }}></div>
+		<div>
+			<motion.button
+				layout
+				onClick={() => setIsVisible((prev) => !prev)}
+				style={{
+					backgroundColor: 'cyan',
+					color: 'white',
+					fontWeight: 700,
+					padding: '10px 20px',
+					borderRadius: 15,
+					marginBottom: 40,
+				}}>
+				Show/Hide
+			</motion.button>
+			<AnimatePresence mode='popLayout'>
+				{isVisible && (
+					<motion.div
+						initial={{ rotate: '0deg' }}
+						animate={{
+							rotate: '180deg',
+							y: [0, 30, -150, -150, 0],
+						}}
+						exit={{ rotate: '0deg' }}
+						transition={{
+							duration: 3,
+							ease: 'backInOut',
+							times: [0, 0.8, 0.9, 0.95, 1],
+						}}
+						style={{
+							height: 150,
+							width: 150,
+							backgroundColor: 'black',
+						}}></motion.div>
+				)}
+			</AnimatePresence>
+		</div>
 	);
 }
