32811926db4a9142338a243e027e8002d7c39a84
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-11-06 16:56:52 +0600

    created horizontal scroll carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,39 @@ import {
 	AnimationControls,
 	BasicsOfFramerMotion,
 	Gestures,
+	HorizontalScrollCarousel,
 	ScrollBaseAnimation,
 	ViewBaseAnimation,
 } from './components';
 
 function App() {
 	return (
-		<div
-			style={{
-				minHeight: '100vh',
-				backgroundColor: 'gray',
-				display: 'grid',
-				placeItems: 'center',
-			}}>
-			{/* <BasicsOfFramerMotion /> */}
-			{/* <Gestures /> */}
-			{/* <AnimationControls /> */}
-			{/* <ViewBaseAnimation /> */}
-			<ScrollBaseAnimation />
+		// <div
+		// 	style={{
+		// 		minHeight: '100vh',
+		// 		backgroundColor: 'gray',
+		// 		display: 'grid',
+		// 		placeItems: 'center',
+		// 	}}>
+		// 	{/* <BasicsOfFramerMotion /> */}
+		// 	{/* <Gestures /> */}
+		// 	{/* <AnimationControls /> */}
+		// 	{/* <ViewBaseAnimation /> */}
+		// 	{/* <ScrollBaseAnimation /> */}
+		// </div>
+
+		<div className='bg-neutral-800'>
+			<div className='flex h-48 items-center justify-center'>
+				<span className='font-semibold uppercase text-neutral-500'>
+					Scroll down
+				</span>
+			</div>
+			<HorizontalScrollCarousel />
+			<div className='flex h-48 items-center justify-center'>
+				<span className='font-semibold uppercase text-neutral-500'>
+					Scroll up
+				</span>
+			</div>
 		</div>
 	);
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as BasicsOfFramerMotion } from './BasicsOfFramerMotion';
 export { default as AnimationControls } from './AnimationControls';
 export { default as ViewBaseAnimation } from './ViewBaseAnimation';
 export { default as ScrollBaseAnimation } from './ScrollBaseAnimation';
+export { default as HorizontalScrollCarousel } from './HorizontalScrollCarousel';
